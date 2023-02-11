30e58cf47cdf39c7b4d9f33b3287a3e3f680f1d7
Tolulope <emryzs01@gmail.com>
2023-02-12 02:00:03 +0300

    Finished writing test

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("@nomiclabs/hardhat-ethers");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
   networks: {
     goerli: {
       url: process.env.ALCHEMY_GOERLI_URL,
